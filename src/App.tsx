f80c38146a34d11ee19fa5f6744827221dc66494
juliabush <j.elizabethbush@gmail.com>
2025-07-30 21:36:27 +0200

    Minor chane to image imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-// import { thefool } from "./assets/Tarot_00_Fool.webp";
+// import {} from "./assets/Tarot_00_Fool.webp";
 // import {} from "./assets/2nd card.webp";
 // import {} from "./assets/Tarot_03_Empress.webp";
 import "./App.css";
