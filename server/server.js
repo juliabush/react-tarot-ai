5394fd02b5af100688b9795c83cb07bd2060ebb3
juliabush <j.elizabethbush@gmail.com>
2025-08-03 23:00:46 +0200

    Fixing file path of .env and editing server.js to be type commonjs

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 const app = express();
-import { config } from "./.gitignore/.env";
-config();
 
 app.get("/", (req, res) => res.send("Hello"));
 
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(process.env.API_URL);
+  console.log(process.env.API_KEY);
 });
