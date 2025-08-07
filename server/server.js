0210007ffa7885deaa66434552efb6e697fde587
juliabush <j.elizabethbush@gmail.com>
2025-08-07 23:31:43 +0200

    Adding chatgpt basic developer boilerplate

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
-const express = require("express");
-const dotenv = require("dotenv");
+import express from "express";
+import dotenv from "dotenv";
+import OpenAI from "openai";
+const client = new OpenAI();
 
 dotenv.config();
 
@@ -11,3 +13,11 @@ const PORT = 3000;
 app.listen(PORT, () => {
   console.log(process.env.API_KEY);
 });
+
+const response = await client.responses.create({
+  model: "gpt-5",
+  input: "Write a short bedtime story about a unicorn.",
+});
+
+console.log(response.output_text);
+console.log(dog);
