ce8dee0bc1dd85774d32c37dbf19c355fa109d6d
juliabush <j.elizabethbush@gmail.com>
2025-08-08 20:02:20 +0200

    API key working

--- server/server.js
+++ server/server.js
@@ -6,14 +6,13 @@ const client = new OpenAI({ apiKey: process.env.API_KEY });
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello"));
-
-const PORT = 3000;
-app.listen(PORT, () => {});
-
 const response = await client.responses.create({
   model: "gpt-5",
   input: "Write a short bedtime story about a unicorn.",
 });
-
 console.log(response.output_text);
+
+app.get("/", (req, res) => res.send());
+
+const PORT = 3000;
+app.listen(PORT, () => {});
